6555f1160aa69923c21b6c04f1afc5c338e39233
PAVLO TESLENKO <149189014+TeslenkoPavlo@users.noreply.github.com>
2025-04-26 18:56:57 +0300

    Add files via upload

--- src/domain/Editor.java
+++ src/domain/Editor.java
@@ -1,28 +1,81 @@
 package domain;
+
+ /**
+ * The class Editor extends artist
+ */ 
 public class Editor extends Artist {
 
-    public Editor(boolean electronicEditing, String[] skiils, String name, String jobTitle, int level, String dept) {
+
+/** 
+ *
+ * Editor
+ *
+ * @param electronicEditing  the electronic editing. 
+ * @param skiils  the skiils. 
+ * @param name  the name. 
+ * @param jobTitle  the job title. 
+ * @param level  the level. 
+ * @param dept  the dept. 
+ * @return public
+ */
+    public Editor(boolean electronicEditing, String[] skiils, String name, String jobTitle, int level, String dept) { 
+
         super(skiils, name, jobTitle, level, dept);
         this.electronicEditing = electronicEditing;
     }
 
-    public Editor(boolean electronicEditing, String[] skiils) {
+
+/** 
+ *
+ * Editor
+ *
+ * @param electronicEditing  the electronic editing. 
+ * @param skiils  the skiils. 
+ * @return public
+ */
+    public Editor(boolean electronicEditing, String[] skiils) { 
+
         super(skiils);
         this.electronicEditing = electronicEditing;
     }
 
-    public Editor(boolean electronicEditing) {
+
+/** 
+ *
+ * Editor
+ *
+ * @param electronicEditing  the electronic editing. 
+ * @return public
+ */
+    public Editor(boolean electronicEditing) { 
+
         super();
         this.electronicEditing = electronicEditing;
     }
     
-    public Editor() {
+
+/** 
+ *
+ * Editor
+ *
+ * @return public
+ */
+    public Editor() { 
+
         super();
         this.electronicEditing = true;
     }
 
     @Override
-    public String toString() {
+
+/** 
+ *
+ * To string
+ *
+ * @return String
+ */
+    public String toString() { 
+
         String s=super.toString()+"\nEditing preferences= "; 
         if (electronicEditing)
             s=s+"electronic";
@@ -34,11 +87,27 @@ public class Editor extends Artist {
 
     private boolean electronicEditing;
 
-    public boolean getPreferences() {
+
+/** 
+ *
+ * Gets the preferences
+ *
+ * @return the preferences
+ */
+    public boolean getPreferences() { 
+
         return electronicEditing;
     }
 
-    public void setPreferences(boolean electronic) {
+
+/** 
+ *
+ * Sets the preferences
+ *
+ * @param electronic  the electronic. 
+ */
+    public void setPreferences(boolean electronic) { 
+
         this.electronicEditing=electronic;
     }
 }
